358ef2393eaa6eef5b262bb4dd077857c27aaa81
Jorge Ramirez <jorgeramirez927@gmail.com>
2026-03-15 21:45:31 -0400

    export ecr_env vars so docker-compose can read them

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,9 @@ set -e
 cd /home/ubuntu/nocableapp
 
 # Load ECR env vars written by CodeBuild
+set -a
 source ecr_env
+set +a
 
 # Authenticate and pull
 aws ecr get-login-password --region $AWS_DEFAULT_REGION \
